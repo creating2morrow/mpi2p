acb2a6695f4e68117704e15f7792b88df017477e
creating2morrow <creating_tomorrow@proton.me>
2023-02-26 02:08:54 -0500

    start order modification api

--- src/order.rs
+++ src/order.rs
@@ -1,4 +1,6 @@
 use crate::models::*;
+use crate::logger;
+use crate::schema;
 use crate::utils;
 use diesel::prelude::*;
 
@@ -27,8 +29,7 @@ impl OrderStatusType {
     }
 }
 
-#[derive(Debug)]
-pub enum OrderUpdateType {
+enum OrderUpdateType {
     CustomerKex1,
     CustomerKex2,
     CustomerKex3,
@@ -44,6 +45,28 @@ pub enum OrderUpdateType {
     VendorKex3,
     VendorMultisigInfo,
     Quantity,
+}
+
+impl OrderUpdateType {
+    pub fn value(&self) -> i32 {
+        match *self {
+            OrderUpdateType::CustomerKex1 => 0,
+            OrderUpdateType::CustomerKex2 => 1,
+            OrderUpdateType::CustomerKex3 => 2,
+            OrderUpdateType::CustomerMultisigInfo => 3,
+            OrderUpdateType::Deliver => 3,
+            OrderUpdateType::Hash => 3,
+            OrderUpdateType::SignMultisig => 3,
+            OrderUpdateType::Ship => 3,
+            OrderUpdateType::SubmitMultisig => 3,
+            OrderUpdateType::Status => 3,
+            OrderUpdateType::VendorKex1 => 3,
+            OrderUpdateType::VendorKex2 => 3,
+            OrderUpdateType::VendorKex3 => 3,
+            OrderUpdateType::VendorMultisigInfo => 3,
+            OrderUpdateType::Quantity => 3,
+        }
+    }
 }
 
 /// Create a skeleton for order
@@ -80,4 +103,39 @@ pub async fn create(cid: String, pid: String) -> Order {
         .values(&new_order)
         .get_result(connection)
         .expect("Error saving new order")
+}
+
+pub async fn modify(_id: String, data: String, update_type: i32) -> Product {
+    use self::schema::orders::dsl::*;
+    let connection = &mut utils::establish_pgdb_connection().await;
+    
+    if update_type == OrderUpdateType::CustomerKex1.value() {
+        logger::log(logger::LogLevel::INFO, "Modify order customer kex 1.").await;
+        let m = diesel::update(orders.find(_id))
+            .set(o_cust_kex_1.eq(data))
+            .get_result::<Order>(connection);
+        match m {
+            Ok(m) => m,
+            Err(_e) => Default::default(),
+        };
+    } else if update_type == OrderUpdateType::CustomerKex2.value() {
+        logger::log(logger::LogLevel::INFO, "Modify customer kex 2.").await;
+        let m = diesel::update(orders.find(_id))
+            .set(o_cust_kex_2.eq(data))
+            .get_result::<Order>(connection);
+        match m {
+            Ok(m) => m,
+            Err(_e) => Default::default(),
+        };
+    } else if update_type == OrderUpdateType::CustomerKex3.value() {
+        logger::log(logger::LogLevel::INFO, "Modify customer kex 3.").await;
+        let m = diesel::update(orders.find(_id))
+            .set(o_cust_kex_3.eq(data))
+            .get_result::<Order>(connection);
+        match m {
+            Ok(m) => m,
+            Err(_e) => Default::default(),
+        };
+    }
+    Default::default()
 }
